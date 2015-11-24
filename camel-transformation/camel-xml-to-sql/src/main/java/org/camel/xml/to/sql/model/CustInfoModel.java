04385848900786f0a2c5d52279cfc99812b8871e
taoef1k <taoef1k1604@gmail.com>
2015-11-24 17:12:06 +0700

    Added Camel REST with SQL

---
+++ camel-transformation/camel-xml-to-sql/src/main/java/org/camel/xml/to/sql/model/CustInfoModel.java
@@ -0,0 +1,177 @@
+package org.camel.xml.to.sql.model;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CustInfoModel.
+ */
+public class CustInfoModel {
+	
+    /** The process. */
+    private String process;
+    
+    /** The national id. */
+    private String nationalID;
+    
+    /** The first name. */
+    private String firstName;
+    
+    /** The last name. */
+    private String lastName;
+    
+    /** The age. */
+    private int age;
+    
+    /** The occupation. */
+    private String occupation;
+    
+    /** The email. */
+    private String email;
+    
+    /** The infotype. */
+    private String infotype;
+    
+	/**
+	 * Gets the process.
+	 *
+	 * @return the process
+	 */
+	public String getProcess() {
+		return process;
+	}
+	
+	/**
+	 * Sets the process.
+	 *
+	 * @param process the new process
+	 */
+	public void setProcess(String process) {
+		this.process = process;
+	}
+	
+	/**
+	 * Gets the national id.
+	 *
+	 * @return the national id
+	 */
+	public String getNationalID() {
+		return nationalID;
+	}
+	
+	/**
+	 * Sets the national id.
+	 *
+	 * @param nationalID the new national id
+	 */
+	public void setNationalID(String nationalID) {
+		this.nationalID = nationalID;
+	}
+	
+	/**
+	 * Gets the first name.
+	 *
+	 * @return the first name
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	/**
+	 * Sets the first name.
+	 *
+	 * @param firstName the new first name
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * Gets the last name.
+	 *
+	 * @return the last name
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * Sets the last name.
+	 *
+	 * @param lastName the new last name
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * Gets the age.
+	 *
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	
+	/**
+	 * Sets the age.
+	 *
+	 * @param age the new age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	/**
+	 * Gets the occupation.
+	 *
+	 * @return the occupation
+	 */
+	public String getOccupation() {
+		return occupation;
+	}
+	
+	/**
+	 * Sets the occupation.
+	 *
+	 * @param occupation the new occupation
+	 */
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	
+	/**
+	 * Gets the email.
+	 *
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
+	/**
+	 * Sets the email.
+	 *
+	 * @param email the new email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	/**
+	 * Gets the infotype.
+	 *
+	 * @return the infotype
+	 */
+	public String getInfotype() {
+		return infotype;
+	}
+	
+	/**
+	 * Sets the infotype.
+	 *
+	 * @param infotype the new infotype
+	 */
+	public void setInfotype(String infotype) {
+		this.infotype = infotype;
+	}
+}
